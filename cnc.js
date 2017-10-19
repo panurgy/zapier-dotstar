812e36e6a9adcfd494b9639d20199afd62729b22
bcooksey <bac2k@hotmail.com>
2017-10-19 03:34:59 +0000

    Refactor some cnc code so that it will eventually support reading local settings as well

--- cnc.js
+++ cnc.js
@@ -1,7 +1,6 @@
-var secretKey = process.env.SECRET_KEY;
-if (! secretKey) {
-    console.log("The 'SECRET_KEY' env var is not set, and will not be able to fetch settings from the remote/storage Command and Control");
-}
+var fetch = require('node-fetch');
+var dotstar = require('dotstar');
+var SPI = require('pi-spi');
 
 var SIMPLE_TIMER_LOOP = true;
 if (process.env.SIMPLE_TIMER_LOOP == '0') {
@@ -11,10 +10,7 @@ if (process.env.SIMPLE_TIMER_LOOP == '0') {
 var DEFAULT_BG_COLOR = { r: 200, g: 20, b: 0 };
 var DEFAULT_FG_COLOR = { r: 200, g: 200, b: 200 };
 
-var fetch = require('node-fetch');
-var dotstar = require('dotstar');
-var SPI = require('pi-spi');
- 
+// Setup the device
 spi = SPI.initialize('/dev/spidev0.0');
 var ledStripLength = 251; // there's really 248, but 251 works more reliably
  
@@ -22,7 +18,7 @@ var ledStrip = new dotstar.Dotstar(spi, {
   length: ledStripLength
 });
 
-// monkey-patch the "set" method, so that we can "get" the info later
+// monkey-patch the "set" method so that we can "get" the info later
 var ledInfo = [];
 var realSetFunction = ledStrip.set;
 var overrideSetFunction = function(pos, r, g, b, a) {
@@ -35,7 +31,6 @@ ledStrip.get = function(pos) {
   return ledInfo[pos];
 };
 
-
 var shows = {};
 shows.solid = require('./shows/solid').show;
 shows.marquee = require('./shows/marquee').show;
@@ -47,25 +42,29 @@ shows.walking = require('./shows/walking').show;
 shows.ripple = require('./shows/ripple').show;
 shows.mainframe= require('./shows/mainframe').show;
 
-var makeDefaultSettings = function() {
-
-    return {
-        bgcolor: { r: 200, g: 20, b: 0 },
-        bgshow: 'solid',
-        fgcolor: { r: 200, g: 200, b:200 },
-        fgshow: 'starburst',
-        defaultAlpha: 0.5
-    };
+var currentSettings = {
+    bgcolor: DEFAULT_BG_COLOR,
+    bgshow: 'solid',
+    fgcolor: DEFAULT_FG_COLOR,
+    fgshow: 'starburst',
+    defaultAlpha: 0.5
 };
+var fetchSettings;
 
-// create the default settings, in case the CnC server can't be reached
-var currentSettings = makeDefaultSettings();
-
-
+// Determine how we will get the show settings
+var secretKey = process.env.SECRET_KEY;
+var localSettings = process.env.SHOW_SETTINGS;
+if (secretKey) {
+    console.log('SECRET_KEY set, will attempt to load settings from Command and Control server...');
+} else if (localSettings) {
+    console.log('SHOW_SETTINGS set, will load from the provided file...');
+} else {
+    console.log("The 'SECRET_KEY' or 'SHOW_SETTINGS' env var is not set, falling back to default show settings...");
+}
 
 // fetches the current JSON settings from the CNC server, and if successful,
 //    submits itself to run again a second from now.
-var fetchSettings = function() {
+fetchSettings = function() {
     if (!secretKey) {
         return;
     }
